7bafcbe418f775f6ef8ea3ce5484c6c7679558d9
Thomas Weidner <tomweidner103@gmail.com>
2019-01-30 10:41:49 -0800

    working on bookmarks

---
+++ app/client/store/bookmarks.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { webserver } from "../../../helperfunction";
+
+const ADD_BOOKMARKED = "ADD_BOOKMARKED";
+
+export const addBookmarkToRecipe = bookmark => ({
+  type: ADD_BOOKMARKED,
+  bookmark
+});
+
+export const postRanking = obj => async dispatch => {
+  try {
+    const recipeId = obj.recipeId;
+    const rank = obj.rank;
+    const userId = obj.userId;
+    const newObj = { recipeId, rank };
+    await axios.post(`${webserver}/api/user/${userId}/bookmarks`, newObj);
+    dispatch(addBookmarkToRecipe({userId, rank, recipeId}))
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const addedBookmark = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_BOOKMARKED:
+      return action.bookmarked;
+    default:
+      return state;
+  }
+};
+
+export default addedBookmark;
