be2aa3d906b5b8498ba61cc64ec311d351e18492
Victor Marta <vicm159@gmail.com>
2019-02-03 09:10:41 -0800

    change app/src/main/AndroidManifest

---
+++ app/client/store/yamiDinners.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { webserver } from '../../../helperfunction';
+
+const GET_YAMIDINNER = 'GET_YAMIDINNER';
+
+const default_dinner = [];
--- app/screens/maps.js
+++ app/screens/maps.js
@@ -1,29 +1,65 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { View, StyleSheet } from 'react-native';
 
-const MapComponent = props => {
-  console.log(props);
-  return (
-    <View style={style.mapContainer}>
-      <MapView
-        style={style.map}
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
-    </View>
-  );
-};
+class MapComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      yamiDinners: [],
+    };
+  }
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        return {
+          lat_test: pos.coords.latitude,
+          long_test: pos.coords.longitude,
+        };
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <View style={style.mapContainer}>
+        <MapView
+          style={style.map}
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          region={{
+            latitude: 37.58825,
+            longitude: -122.0432,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+          {this.state.yamiDinners.map(dinner => {
+            return (
+              <Marker
+                key={dinner.id}
+                title={dinner.title}
+                coordinate={dinner.cord}
+                description={dinner.description}
+                onPress={() => {
+                  console.log('this is being pressed', dinner.title);
+                }}
+              />
+            );
+          })}
+        </MapView>
+      </View>
+    );
+  }
+}
 
 const style = StyleSheet.create({
   mapContainer: {
     width: '100%',
-    height: '100%',
+    height: '90%',
   },
   map: {
     width: '100%',
