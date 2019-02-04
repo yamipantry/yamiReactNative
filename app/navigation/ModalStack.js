16f387a0b8cef43be7732daed3c591a1e9969387
Ashley Smith <smitham85@gmail.com>
2019-02-04 14:21:05 -0800

    menu not appearing at login

--- app/navigation/ModalStack.js
+++ app/navigation/ModalStack.js
@@ -1,32 +1,38 @@
-import { createSwitchNavigator } from 'react-navigation'
-import BookmarksModal from '../client/modals/modalExample'
-import Recipes from '../client/recipesContainer'
-import SingleRecipe from '../client/SingleRecipeContainer';
-import Pantry from '../client/PantryContainer';
-import { Settings } from '../screens/settings';
-import profileContainer from '../client/profileContainer';
+import { createSwitchNavigator } from 'react-navigation';
+import BookmarksModal from '../client/modals/modalExample';
 import NoRecipes from '../client/modals/NoRecipes';
 import BookmarksContainer from '../client/BookmarksContainer';
-import yamiMaps from '../screens/yamiMaps'
-import YamiDinners from '../client/yamiDinnerFormContainer'
+import yamiMaps from '../screens/yamiMaps';
+import YamiDinners from '../client/yamiDinnerFormContainer';
+import FriendsPantry from '../screens/friendsPantry';
+import {
+  FriendsStack,
+  PantryStack,
+  ProfileStack,
+  RecipesStack,
+  SingleRecipeStack,
+  SettingsStack,
+} from './singlestack';
 
-
-const ModalStack = createSwitchNavigator( {
-    SettingsScreen: { screen: Settings },
-    ProfileScreen: { screen: profileContainer },
-    BookmarksModal: { screen: BookmarksModal},
-    Recipes: {screen: Recipes},
-    SingleRecipe: {screen: SingleRecipe},
-    Home: { screen: Pantry},
-    NoRecipes: { screen: NoRecipes},
-    Bookmarks: { screen: BookmarksContainer},
-    YamiDinnerFormScreen: {screen: YamiDinners},
-    YamiMaps: {screen: yamiMaps}
-},{
+const ModalStack = createSwitchNavigator(
+  {
+    SettingsScreen: { screen: SettingsStack },
+    ProfileScreen: { screen: ProfileStack },
+    BookmarksModal: { screen: BookmarksModal },
+    Recipes: { screen: RecipesStack },
+    Friends: { screen: FriendsStack },
+    FreindsPantry: { screen: FriendsPantry },
+    SingleRecipe: { screen: SingleRecipeStack },
+    Home: { screen: PantryStack },
+    NoRecipes: { screen: NoRecipes },
+    Bookmarks: { screen: BookmarksContainer },
+    YamiDinnerFormScreen: { screen: YamiDinners },
+    YamiMaps: { screen: yamiMaps },
+  },
+  {
     headerMode: 'none',
     initialRouteName: 'Home',
-    contentComponent: Pantry
-}
-)
+  }
+);
 
-export default ModalStack
+export default ModalStack;
--- app/navigation/drawerNavigation.js
+++ app/navigation/drawerNavigation.js
@@ -1,89 +1,18 @@
-
-import React from "react";
-import { Text, TouchableOpacity, Image } from "react-native";
-import { createStackNavigator, DrawerActions } from "react-navigation";
-import DrawerStack from "./drawerStack";
-import ModalStack from './ModalStack'
-import { webserver } from "../../helperfunction";
-
-
-export class LogoTitle extends React.Component {
-  render() {
-    return (
-      <Image
-        source={{uri: `${webserver}/logo.jpeg`}}
-        style={{ width: 50, height: 50}}
-      />
-    );
-  }
-}
+import React from 'react';
+import { Text, TouchableOpacity, Image } from 'react-native';
+import { createStackNavigator, DrawerActions } from 'react-navigation';
+import DrawerStack from './drawerStack';
+import ModalStack from './ModalStack';
+import { webserver } from '../../helperfunction';
 
 const DrawerNavigation = createStackNavigator(
   {
     DrawerStackScreen: {
       screen: DrawerStack,
-      navigationOptions: ({ navigation }) => ({
-        headerStyle: { backgroundColor: "#FFFFFF" },
-        headerTitle: <LogoTitle />,
-        headerTitleContainerStyle: {justifyContent: 'center'},
-        headerTintColor: "white",
-        headerLeft: (
-          <Text
-            style={{ fontWeight: "300", fontSize: 20, paddingLeft: 40 }}
-            onPress={() => {
-              navigation.dispatch(DrawerActions.toggleDrawer());
-            }}
-          >
-            Menu
-          </Text>
-        ),
-        headerRight: (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("Home", { editMode: true });
-            }}
-          >
-            <Text style={{ fontWeight: "300", fontSize: 20, paddingRight: 15 }}>
-              Edit Pantry
-            </Text>
-          </TouchableOpacity>
-        )
-      })
-    },
-    ScreenStack: {
-      screen: ModalStack,
-      navigationOptions: ({ navigation }) => ({
-        headerStyle: { backgroundColor: "#FFFFFF" },
-        headerTitle: <LogoTitle />,
-        headerTitleContainerStyle: {justifyContent: 'center'},
-        headerTintColor: "white",
-        headerLeft: (
-          <Text
-            style={{ fontWeight: "300", fontSize: 20, paddingLeft: 40 }}
-            onPress={() => {
-              navigation.dispatch(DrawerActions.toggleDrawer());
-            }}
-          >
-            Menu
-          </Text>
-        ),
-        headerRight: (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("Home", { editMode: true });
-            }}
-          >
-            <Text style={{ fontWeight: "300", fontSize: 20, paddingRight: 15 }}>
-              Edit Pantry
-            </Text>
-          </TouchableOpacity>
-        )
-      })
     },
   },
   {
-    headerMode: "screen"
+    headerMode: 'none',
   }
 );
-
 export default DrawerNavigation;
