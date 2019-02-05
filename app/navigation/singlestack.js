6530d8c36bd59d4987581b7a686a6bf326036558
Victor Marta <vicm159@gmail.com>
2019-02-04 18:59:04 -0800

    cleanup

--- app/navigation/singlestack.js
+++ app/navigation/singlestack.js
@@ -7,7 +7,7 @@ import {
 } from 'react-navigation';
 import { Settings } from '../screens/settings';
 import profileContainer from '../client/profileContainer';
-import BookmarksContainer from "../client/BookmarksContainer";
+import BookmarksContainer from '../client/BookmarksContainer';
 import Pantry from '../client/PantryContainer';
 import Recipes from '../client/recipesContainer';
 import SingleRecipe from '../client/SingleRecipeContainer';
@@ -18,20 +18,19 @@ import YamiDinnersCont from '../client/yamiDinnerFormContainer';
 import YamiMaps from '../screens/yamiMaps';
 import styles from '../assets/styles';
 
-
 export const PantryStack = createStackNavigator({
   Home: {
     screen: Pantry,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Pantry",
+      headerMode: 'screen',
+      headerTitle: 'Pantry',
       headerTitleStyle: {
         flex: 1,
-        textAlign: "center",
-        alignSelf: "center"
+        textAlign: 'center',
+        alignSelf: 'center',
       },
       headerLeft: (
-        <View style={{paddingLeft: 25}}>
+        <View style={{ paddingLeft: 25 }}>
           <RkText
             style={styles.menuText}
             onPress={() => {
@@ -43,34 +42,34 @@ export const PantryStack = createStackNavigator({
         </View>
       ),
       headerRight: (
-        <View style={{paddingRight: 20}}>
+        <View style={{ paddingRight: 20 }}>
           <RkButton
             rkType="outline small"
             style={{ width: 50 }}
             onPress={() => {
-              navigation.navigate("Home", { editMode: true });
+              navigation.navigate('Home', { editMode: true });
             }}
           >
             Edit Pantry
           </RkButton>
         </View>
-      )
-    })
-  }
+      ),
+    }),
+  },
 });
 
 export const SettingsStack = createStackNavigator({
   SettingsScreen: {
     screen: Settings,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Settings",
+      headerMode: 'screen',
+      headerTitle: 'Settings',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <RkText
           style={styles.menuText}
@@ -80,23 +79,23 @@ export const SettingsStack = createStackNavigator({
         >
           Menu
         </RkText>
-      )
-    })
-  }
+      ),
+    }),
+  },
 });
 
 export const ProfileStack = createStackNavigator({
   ProfileScreen: {
     screen: profileContainer,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Profile",
+      headerMode: 'screen',
+      headerTitle: 'Profile',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <RkText
           style={styles.menuText}
@@ -106,23 +105,23 @@ export const ProfileStack = createStackNavigator({
         >
           Menu
         </RkText>
-      )
-    })
-  }
+      ),
+    }),
+  },
 });
 
 export const BookmarkStack = createStackNavigator({
   BookmarksContainer: {
     screen: BookmarksContainer,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Bookmarked Recipes",
+      headerMode: 'screen',
+      headerTitle: 'Bookmarked Recipes',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <RkText
           style={styles.menuText}
@@ -132,23 +131,23 @@ export const BookmarkStack = createStackNavigator({
         >
           Menu
         </RkText>
-      )
-    })
-  }
+      ),
+    }),
+  },
 });
 
 export const RecipesStack = createStackNavigator({
   Recipes: {
     screen: Recipes,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Suggested Recipes",
+      headerMode: 'screen',
+      headerTitle: 'Suggested Recipes',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <RkText
           style={styles.menuText}
@@ -158,20 +157,20 @@ export const RecipesStack = createStackNavigator({
         >
           Menu
         </RkText>
-      )
-    })
+      ),
+    }),
   },
   SingleRecipe: {
     screen: SingleRecipe,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "Recipe",
+      headerMode: 'screen',
+      headerTitle: 'Recipe',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <RkText
           style={styles.menuText}
@@ -181,20 +180,20 @@ export const RecipesStack = createStackNavigator({
         >
           Menu
         </RkText>
-      )
-    })
+      ),
+    }),
   },
   YamiDinners: {
     screen: YamiDinnersCont,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "YAMI Dinners",
+      headerMode: 'screen',
+      headerTitle: 'YAMI Dinners',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <View>
           <RkText
@@ -209,7 +208,8 @@ export const RecipesStack = createStackNavigator({
       ),
     }),
   },
-  //we deleted here so maybe misssing curly brace or paraenthesis
+});
+//we deleted here so maybe misssing curly brace or paraenthesis
 
 export const FriendsStack = createStackNavigator({
   Friends: {
@@ -295,14 +295,14 @@ export const YamiMapStack = createStackNavigator({
   YamiMap: {
     screen: YamiMaps,
     navigationOptions: ({ navigation }) => ({
-      headerMode: "screen",
-      headerTitle: "YAMI Maps",
+      headerMode: 'screen',
+      headerTitle: 'YAMI Maps',
       headerTitleStyle: {
-        alignSelf: "center",
-        textAlign: "center",
-        flexGrow: 1
+        alignSelf: 'center',
+        textAlign: 'center',
+        flexGrow: 1,
       },
-      headerRight: <View style={{paddingLeft: 25}} />,
+      headerRight: <View style={{ paddingLeft: 25 }} />,
       headerLeft: (
         <View>
           <RkText
@@ -314,7 +314,7 @@ export const YamiMapStack = createStackNavigator({
             Menu
           </RkText>
         </View>
-      )
-    })
-  }
+      ),
+    }),
+  },
 });
